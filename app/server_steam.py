24c235b93507eea08e47f0a08b81ff6fbf161a3c
Housaku <tindalos11@gmail.com>
2024-03-04 21:30:14 +0900

    接続毎に送信バッファを持つように修正

--- app/server_steam.py
+++ app/server_steam.py
@@ -8,7 +8,6 @@ import sys
 class AsyncServer:
     buffer_size: int
     is_running: bool = True
-    send_list: list[str] = []
     server: asyncio.Server
 
     def __init__(self, buffer_size: int = 1024):
@@ -38,16 +37,27 @@ class AsyncServer:
     async def handle_echo(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
         addr = writer.get_extra_info("peername")
         print(f"Server connected client: {addr}")
-        asyncio.create_task(self.handle_recv(reader))
-        asyncio.create_task(self.handle_send(writer))
+        tx_buffers: list[str] = []
+        asyncio.create_task(self.handle_recv(reader, tx_buffers))
+        asyncio.create_task(self.handle_send(writer, tx_buffers))
+        asyncio.create_task(self.handle_cycle(tx_buffers))
 
-    async def handle_send(self, writer: asyncio.StreamWriter) -> None:
+    async def handle_cycle(self, tx_buffers: list[str]) -> None:
+        count = 0
+        while self.is_running:
+            await asyncio.sleep(1.0)
+            count += 1
+            if count % 10 == 0:
+                message = f"{count} seconds have passed since server connection."
+                tx_buffers.append(message)
+
+    async def handle_send(self, writer: asyncio.StreamWriter, tx_buffers: list[str]) -> None:
         """送信処理を行うタスク"""
         while self.is_running:
             await asyncio.sleep(0.2)
-            if self.send_list:
+            if tx_buffers:
                 try:
-                    message = self.send_list.pop(0)
+                    message = tx_buffers.pop(0)
                     writer.write(message.encode("utf-8"))
                     await writer.drain()
                     print(f"TX: {message}")
@@ -61,7 +71,7 @@ class AsyncServer:
         writer.close()
         await writer.wait_closed()
 
-    async def handle_recv(self, reader: asyncio.StreamReader) -> None:
+    async def handle_recv(self, reader: asyncio.StreamReader, tx_buffers: list[str]) -> None:
         """受信処理を行うタスク"""
         while self.is_running:
             try:
@@ -74,17 +84,17 @@ class AsyncServer:
                 break
             # 相手側が落ちると空電文が届くので終了する
             if 0 < len(recv_bytes):
-                self.respond(recv_bytes)
+                self.respond(recv_bytes, tx_buffers)
             else:
                 print("disconnected from client!!!")
                 self.close()
                 return
 
-    def respond(self, recv_bytes: bytes) -> None:
+    def respond(self, recv_bytes: bytes, tx_buffers: list[str]) -> None:
         """応答処理"""
         print(f"RX: {recv_bytes.decode('utf-8')}")
         message = f"echo {recv_bytes.decode('utf-8')!r}"
-        self.send_list.append(message)
+        tx_buffers.append(message)
 
     def close(self) -> None:
         self.is_running = False
